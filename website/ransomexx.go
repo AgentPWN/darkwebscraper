5b86914cfca50b33867c341220c0d8e692ccb314
AgentPWN <rupakbanerjee170@gmail.com>
2026-04-06 11:51:01 +0530

    had not implemented scanning through all the pages last time. Did that this time

--- website/ransomexx.go
+++ website/ransomexx.go
@@ -76,7 +76,37 @@ func Ransomexx(query string) bool {
 			}
 		}
 	}
+	for i := range 7 {
+		ransomexxOnionCurrent := fmt.Sprintf("%sindex%d.html", ransomexxOnion, i+1)
+		req, _ := http.NewRequest("GET", ransomexxOnionCurrent, nil)
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0")
+		req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		req.Header.Set("Accept-Language", "en-US,en;q=0.5")
+		// req.Header.Set("Accept-Encoding", "gzip")
 
+		resp, err := ransomexxClient.Do(req)
+
+		if err != nil {
+			fmt.Println("[Ransomexx] request failed:", err)
+		}
+		bodyBytesRansomexx, err = io.ReadAll(resp.Body)
+		body := string(bodyBytesRansomexx)
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Println(body)
+		if strings.Contains(body, query) {
+			result = true
+			links := utils.ExtractPostLinks(body, "")
+			for _, link := range links {
+				// fmt.Println(link)
+				if strings.Contains(link, strings.ToLower(query)) {
+					fmt.Println(ransomexxOnion + link)
+					result = true
+				}
+			}
+		}
+	}
 	if result == true {
 		fmt.Println("[Ransomexx] Results found")
 		return result
